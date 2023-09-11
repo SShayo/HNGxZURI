7fe944c72e6f17c7f410e1bc161f25d81f040cfd
Shonde Feyisayo <91834564+SShayo@users.noreply.github.com>
2023-09-11 11:20:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get the current day of the week.
-const dayOfWeek = new Date().toLocaleDateString("en-UK", { weekday: "long" });
-
-// Get the current UTC time in milliseconds.
-const utcTime = new Date().getTime();
-
-// Update the DOM with the current day of the week and UTC time.
-document.querySelector("[data-testid='currentDayOfTheWeek']").innerHTML = `Current Day of the Week: ${dayOfWeek}`;
-document.querySelector("[data-testid='currentUTCTime']").innerHTML = `Current UTC Time: ${utcTime}`;
